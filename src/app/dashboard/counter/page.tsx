a7468063e49f76b511cdde30ba0adc855e52f0dc
Bryam Talledo <bryam.talledo@outlook.com>
2025-02-13 13:21:24 -0500

    useState

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,3 +1,17 @@
+import { CartCounter } from '@/app/shopping-cart/components/CartCounter';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Shopping Cart',
+  description: 'Counter Page',
+};
+
 export default function CounterPage() {
-  return <h1>Counter Page</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full">
+      <h1>Counter Page</h1>
+      <span>Productos en el carrito</span>
+      <CartCounter value={20} />
+    </div>
+  );
 }
