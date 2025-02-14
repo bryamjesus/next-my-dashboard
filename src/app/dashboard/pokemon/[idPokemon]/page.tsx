0ac6deb25ccbcf51e24b70834301dc40588070cc
Bryam Talledo <bryam.talledo@outlook.com>
2025-02-14 13:17:48 -0500

    Metada dinamic

--- src/app/dashboard/pokemon/[idPokemon]/page.tsx
+++ src/app/dashboard/pokemon/[idPokemon]/page.tsx
@@ -10,11 +10,23 @@ const getPokemon = async (idPokemon: string): Promise<Pokemon> => {
     `https://pokeapi.co/api/v2/pokemon/${idPokemon}`,
     {
       cache: 'force-cache', // TODO: Cannot find name 'await'.
+      // next:{
+      //   revalidate: 60 * 60 * 30 * 6 // TODO: 6 meses
+      // }
     }
   ).then((response) => response.json());
   console.log(`Se cargó: ${pokemon.name}`);
   return pokemon;
 };
+
+// TODO: Metadata dinámica
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id, name } = await getPokemon(params.idPokemon);
+  return {
+    title: `#${id} - ${name}`,
+    description: `Información del pokemon ${name}`,
+  };
+}
 
 export default async function PokemonPage({ params }: Props) {
   // Necesita el awit por que no llega el valor siempre
@@ -24,7 +36,7 @@ export default async function PokemonPage({ params }: Props) {
   return (
     <div>
       <h1>Pokemon {idPokemon}</h1>
-      <div>{JSON.stringify(pokemon)}</div>
+      <div>{JSON.stringify(pokemon.name)}</div>
     </div>
   );
 }
