d7e316da6cc148e2ed2c51711adba2caae15af47
Bryam Talledo <bryam.talledo@outlook.com>
2025-02-22 12:25:16 -0500

    Update config redux

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,4 +1,10 @@
 import { SimpleWidget } from '@/components/SimpleWidget';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Admin Dashboard',
+  description: 'Dashboard',
+};
 
 export default function MainPage() {
   return (
